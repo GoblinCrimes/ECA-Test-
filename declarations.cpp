ff18e9b8621176fa293de318bcf888df24d4e741
GoblinCrimes <140443058+GoblinCrimes@users.noreply.github.com>
2025-01-31 11:20:04 -0300

    Update declarations.cpp

--- declarations.cpp
+++ declarations.cpp
@@ -6,7 +6,7 @@ int cellsLength = {230}; //lenght of each generation in cells, ALSO used for the
 int cells[230] = {}; //array holding the current generation of cells
 int nextCells[230] = {}; //array to temporalily hold the next generation of cells before being copied into cells[]
 int ruleNumber = {1}; //number that represents and is used to iterate through the rules SHITS ITSELF WHEN = {0}
-int ruleSet[8] = {0, 0, 0, 1, 1, 1, 1, 0}; // holds the current ruletset in an array to be read by calculateState()
+int ruleSet[8] = {0, 0, 0, 0, 0, 0, 0, 0}; // holds the current ruletset in an array to be read by calculateState()
 
 
 void changeRuleset() //convert ruleNumber to binary as a string, then take the digits of that binary number and assign them to the indexes of ruleSet
