01bb753f0289ddbb2db8bfec67b0c046c24e0be5
GoblinCrimes <140443058+GoblinCrimes@users.noreply.github.com>
2025-01-31 11:19:01 -0300

    Update helloworld.h

--- helloworld.h
+++ helloworld.h
@@ -4,7 +4,7 @@
 void initializeCells(); //
 void drawCells(); //Iterates through the cells array to print it out
 int calculateState(int a, int b, int c); //Gets the next cell state for the next generation
-void calculateGeneration(); 
-void changeRuleset();
+void calculateGeneration(); //Takes the neighbors of a cell and passes their states to calculateState() 
+void changeRuleset(); //If drawing more than one rule, changes to the next one after one loop of main
 
 #endif
