9d636d542cefa5957f4e16fd94a239cf8c15b117
GoblinCrimes <140443058+GoblinCrimes@users.noreply.github.com>
2025-01-31 10:51:45 -0300

    helloworld.cpp

--- helloworld.cpp
+++ helloworld.cpp
@@ -1,7 +1,5 @@
 #include <iostream>
 #include "helloworld.h"
-#include <chrono> 
-#include <thread> 
 int main() //for every individual rule, then for each individual generation, after that change the ruleset to the next
 {
     for (int j = 0; j <= 0; j++) 
@@ -9,9 +7,7 @@ int main() //for every individual rule, then for each individual generation, aft
         initializeCells();
         for (int i = 0; i <= 1000; i++)
         {
-            std::this_thread::sleep_for(std::chrono::milliseconds(500));
             drawCells();
-            //offsetIncrease();
             calculateGeneration();
         }
     changeRuleset(); 
