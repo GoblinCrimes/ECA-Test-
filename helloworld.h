e7e1db20ec5ffd050f15236d4624d0c8b58ae783
GoblinCrimes <140443058+GoblinCrimes@users.noreply.github.com>
2025-01-31 11:15:15 -0300

    helloworld.h

--- helloworld.h
+++ helloworld.h
@@ -1,11 +1,10 @@
 #ifndef HELLOWORLD
 #define HELLOWORLD
 
-void initializeCells();
-void drawCells();
-int calculateState(int a, int b, int c);
-void calculateGeneration();
+void initializeCells(); //
+void drawCells(); //Iterates through the cells array to print it out
+int calculateState(int a, int b, int c); //Gets the next cell state for the next generation
+void calculateGeneration(); 
 void changeRuleset();
-void offsetIncrease();
 
 #endif
